9549023e360cc76a2c91478f3696028ac95b8184
Tomas Susanka <tsusanka@gmail.com>
2012-10-30 20:04:17 +0100

    First setup with Hello World

---
+++ src/bartender/Bartender.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bartender;
+
+/**
+ *
+ * @author Tomáš Sušánka
+ */
+public class Bartender {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
